f4021a03b8c57fa929d115f15affeb1b746aa703
Dhathri33 <dbathini@luc.edu>
2025-10-22 11:05:25 -0500

    Initial Commit

---
+++ RandomUserGeneratorSwiftUI/RandomUserGeneratorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserGeneratorSwiftUIApp.swift
+//  RandomUserGeneratorSwiftUI
+//
+//  Created by Dhathri Bathini on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserGeneratorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
